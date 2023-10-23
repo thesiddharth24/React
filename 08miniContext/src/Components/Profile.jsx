54753b3acc615cc107594816ac68e3791642ac33
thesiddharth24 <siddharth20it028@gmail.com>
2023-10-23 19:20:55 +0530

    api context

---
+++ 08miniContext/src/Components/Profile.jsx
@@ -0,0 +1,30 @@
+// import React, { useContext } from 'react'
+// import UserContext from '../Context/UserContext'
+
+
+// //esme value leni hai 
+// function Profile() {
+//     const {user} = useContext(UserContext);
+//     if(!user)return <div>Please login!!</div>
+//     else{
+//         return <div>Welcome {user.username}</div>
+//     }
+// }
+
+
+// export default Profile
+
+
+//////////////////////
+import React, {useContext} from 'react'
+import UserContext from '../context/UserContext'
+
+function Profile() {
+    const {user} = useContext(UserContext)
+    
+    if (!user) return <div>please login</div>
+
+    return <div>Welcome {user.username}</div>
+}
+
+export default Profile
